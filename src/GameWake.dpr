c1067377cf921702d8b8389066079dda9deab893
Philipp Meisberger <team@pm-codeworks.de>
2017-10-08 16:26:57 +0200

    Using .dpr as main project file; Removed .lpr

--- src/GameWake.dpr
+++ src/GameWake.dpr
@@ -1,15 +1,21 @@
 program GameWake;
 
-
-
 {$IFDEF PORTABLE}
 {$R 'sounds.res' 'sounds.rc'}
 {$ENDIF}
 
+{$IFNDEF FPC}
 {$R *.dres}
+{$ENDIF}
 
 uses
-  Vcl.Forms,
+{$IFDEF UNIX}
+  cthreads,
+{$ENDIF}
+{$IFDEF FPC}
+  Interfaces, // this includes the LCL widgetset
+{$ENDIF}
+  Forms,
   GameWakeMain in 'GameWakeMain.pas' {Main},
   GameWakeAPI in 'GameWakeAPI.pas',
   GameWakeOps in 'GameWakeOps.pas' {Options};
@@ -22,7 +28,9 @@ begin
 {$ENDIF}
   Application.Initialize;
   Application.Title := 'Game Wake';
+{$IFDEF MSWINDOWS}
   Application.MainFormOnTaskbar := True;
+{$ENDIF}
   Application.CreateForm(TMain, Main);
   Application.Run;
 end.
